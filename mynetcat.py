a060c84d14fb348260977b957949e65f1c8318b1
gawu <gawu@Ctrip.com>
2017-01-06 11:44:25 +0800

    fix getopt

--- mynetcat.py
+++ mynetcat.py
@@ -7,17 +7,37 @@ import sys
 import socket
 import threading
 
-target = ''
+target = None
 port = 0
 listen = False
-command = ''
+command = None
 execute = False
 
 def usage():
     print './mynetcat.py -t localhost -p 80 -l -c ls -e'
     
+def client_handler(client_sock):
+    client_sock.close()
+            
+def server_loop():
+    global target
+    global port    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if not target:
+        target = '0.0.0.0'
+    sock.bind((target, port))
+    sock.listen(5)
+    
+    print 'listen on %s:%s' % (target, port)
+    
+    while True:
+        client_sock, addr = sock.accept()
+        print 'Accept host: %s, %s' % (addr[0], addr[1])
+        client_thread = threading.Thread(target=client_handler, args=[client_sock,])
+        client_thread.start()
+    
 def main():
-    opts, _ = getopt.getopt(sys.argv[1:], 'h:t:p:l:c:e', longopts=['help', 'target', 'port', 'listen', 'command', 'execute'])
+    opts, _ = getopt.getopt(sys.argv[1:], 'hlt:p:c:e', longopts=['help', 'listen', 'target', 'port', 'command', 'execute'])
     global target
     global port
     global listen
@@ -41,26 +61,14 @@ def main():
             print 'error'
             sys.exit(1)
     
+    if not listen and target:
+        sock = socket.socket(family=AF_INET, type=SOCK_STREAM)
+        while True:
+            s = sys.stdin.read(1024)
+            
+    
     if listen:
         server_loop()
-    
-
-def client_handler(client_sock):
-    client_sock.close()
-            
-def server_loop():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('localhost', port))
-    sock.listen(5)
-    
-    print 'listen on %s:%s' % ('localhost', port)
-    
-    while True:
-        client_sock, addr = sock.accept()
-        print 'Accept host: %s, %s' % (addr[0], addr[1])
-        client_thread = threading.Thread(target=client_handler, args=[client_handler,])
-        client_thread.start()
-
 
 main()
 
