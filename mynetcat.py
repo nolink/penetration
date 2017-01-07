2f03159d20e3904d4d846fb4d0196e4d470d2b78
root <root@localhost.localdomain>
2017-01-07 07:38:02 -0500

    add server loop

--- mynetcat.py
+++ mynetcat.py
@@ -10,15 +10,54 @@ import threading
 target = None
 port = 0
 listen = False
-command = None
-execute = False
+command = False
+execute = ''
+upload_dest = ''
 
 def usage():
-    print './mynetcat.py -t localhost -p 80 -l -c ls -e'
-    
+    print './mynetcat.py -t localhost -p 80 -l -c ls -u -e'
+   
+def run_command(command):
+    command = command.rstrip()
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell = True)
+    except:
+        output = "failed"
+ 
 def client_handler(client_sock):
-    client_sock.close()
-            
+    global upload
+    global execute
+    global command
+    
+    if len(upload_dest):
+        file_buffer = ""
+        while True:
+            data = client_sock.recv(1024)
+            if not data:
+                break
+            else:
+                file_buffer += data
+         try:
+             with open(upload_dest, 'wb') as fd:
+                 fd.write(file_buffer)
+             client_sock.send('succesfully save file to %s\r\n' % upload_dest)
+         except:
+             client_sock.send('failed to save file to %s\r\n' % upload_dest)
+
+    if len(execute):
+        output = run_command(execute)
+        client_sock.send(output)
+
+    if command:
+        while True:
+            client_sock.send("<MYNETCAT:#>")
+            cmd_buffer = ""
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client.recv(1024)
+            resp = run_command(cmd_buffer)
+            client_sock.send(resp)
+             
+        
 def server_loop():
     global target
     global port    
@@ -36,8 +75,32 @@ def server_loop():
         client_thread = threading.Thread(target=client_handler, args=[client_sock,])
         client_thread.start()
     
+def client_sender(buffer):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((target, port))
+        if len(buffer):
+            sock.send(buffer)
+        
+        while True:
+            recv_len = 1
+            resp = ""
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                resp += data
+                if recv_len < 4096:
+                    break
+            print resp
+            buffer = raw_input("")
+            buffer += "\n"
+            sock.send(buffer)
+    except:
+        print 'error, exiting...'
+        sock.close()    
+
 def main():
-    opts, _ = getopt.getopt(sys.argv[1:], 'hlt:p:c:e', longopts=['help', 'listen', 'target', 'port', 'command', 'execute'])
+    opts, _ = getopt.getopt(sys.argv[1:], 'hlt:p:c:u:e', longopts=['help', 'listen', 'target', 'port', 'command','upload', 'execute'])
     global target
     global port
     global listen
@@ -54,17 +117,18 @@ def main():
         elif p in ('-l', '--listen'):
             listen = True
         elif p in ('-c','--command'):
-            command = arg
+            command = True
         elif p in ('-e', '--execute'):
-            execute = True
+            execute = arg
+        elif p in ('-u', '--upload'):
+            upload_dest = arg
         else:
             print 'error'
             sys.exit(1)
     
-    if not listen and target:
-        sock = socket.socket(family=AF_INET, type=SOCK_STREAM)
-        while True:
-            s = sys.stdin.read(1024)
+    if not listen and len(target) and port  > 0:
+        buffer = sys.stdin.read()
+        client_sender(buffer)
             
     
     if listen:
